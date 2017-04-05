0bff8aa6fff7590b3a2e3dc3e1a372c8ee11a3f6
Sudhir Rawat <rawatsudhir@users.noreply.github.com>
2017-04-05 10:38:18 +0530

    Update deviceManager.py

--- deviceManager.py
+++ deviceManager.py
@@ -1,19 +1,3 @@
-"""
-Module Name:  deviceManager.py
-Project:      IoTHubRestSample
-Copyright (c) Microsoft Corporation.
-
-Using [Device Indentities REST APIs](https://msdn.microsoft.com/en-us/library/azure/mt548489.aspx) to create a new device identity, retrieve a device identity, and list device identities.
-
-This source is subject to the Microsoft Public License.
-See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL
-All other rights reserved.
-
-THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
-EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
-"""
-
 import base64
 import hmac
 import hashlib
